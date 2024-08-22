a69bd848a5ff05eb4069c9dd2d18d6e077e53fd1
JogadorZero <jz.jogadorzero@gmail.com>
2024-08-22 20:13:12 -0300

    Add Ex01
    Adicionando exercício 01

---
+++ exercicio01/Exercicio01.java
@@ -0,0 +1,38 @@
+package main.resources;
+
+
+import java.util.Scanner;
+
+public class Exercicio01 {
+    public static void main(String[] args) {
+
+        while (true) {
+
+            Scanner genero = new Scanner(System.in);
+            System.out.println("Que ambientação você prefere?\n" + "1. Sci-fi\n" + "2. Medieval\n");
+
+            String primeiro = genero.nextLine();
+
+            System.out.println("Que gênero você prefere?\n" + "1. Comédia\n" + "2. Drama\n");
+            String segundo = genero.nextLine();
+
+            if (primeiro.equals("1") && segundo.equals("1")) {
+                System.out.println("Homens de Preto");
+                break;
+            } else if (primeiro.equals("1") && segundo.equals("2")) {
+                System.out.println("Arrival");
+                break;
+            } else if (primeiro.equals("2") && segundo.equals("1")) {
+                System.out.println("Sherk");
+                break;
+            } else if (primeiro.equals("2") && segundo.equals("2")) {
+                System.out.println("Gladiador");
+                break;
+            } else {
+                System.out.println("!!!!!! Digite apenas 1 ou 2 !!!!!!!");
+            }
+
+
+        }
+    }
+}
